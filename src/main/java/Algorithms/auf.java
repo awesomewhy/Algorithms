d440102aa908cdb3cd779e16c88d9fd5f8776c04
serenty <serenty395@gmail.com>
2023-08-31 16:08:07 +0300

    Tretiy commit

---
+++ src/main/java/Algorithms/auf.java
@@ -0,0 +1,39 @@
+package Algorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class auf  {
+    public static void main(String[] args) {
+        int[] y = {10, 1, 2, 7, 6, 1, 5};
+        List<List<Integer>> result = combinationSum2(y, 8);
+        for (List<Integer> list : result) {
+            System.out.println(list);
+        }
+    }
+
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        return result;
+    }
+
+    public static void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] candidates, int remain, int start) {
+        if (remain < 0) {
+            return;
+        } else if (remain == 0) {
+            result.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = start; i < candidates.length; i++) {
+                if (i > start && candidates[i] == candidates[i - 1]) {
+                    continue;
+                }
+                tempList.add(candidates[i]);
+                backtrack(result, tempList, candidates, remain - candidates[i], i + 1);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
