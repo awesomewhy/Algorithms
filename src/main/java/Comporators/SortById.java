e25b91340464c6ad5cdc704c72f55c75dcb0f6c0
serenty <serenty395@gmail.com>
2023-09-07 15:48:19 +0300

    s

--- src/main/java/Comporators/SortById.java
+++ src/main/java/Comporators/SortById.java
@@ -1,11 +1,16 @@
 package Comporators;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 public class SortById implements Comparator<Person> {
 
     @Override
     public int compare(Person o1, Person o2) {
+        List<Integer> list = new ArrayList<>();
+
+
         if(o1.getId() == o2.getId())
         {
             return 0;
