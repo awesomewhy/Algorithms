0ced497fc54bdf1a2b4636ff1b87612d4fd62704
serenty <serenty395@gmail.com>
2023-10-02 19:46:30 +0300

    asd

--- src/main/java/Algorithms/Algorithms.java
+++ src/main/java/Algorithms/Algorithms.java
@@ -20,7 +20,7 @@ public class Algorithms {
                 ind++;
             }
             else {
-                break;
+                break;//asd
             }
         }
         return s1.substring(0,ind);
