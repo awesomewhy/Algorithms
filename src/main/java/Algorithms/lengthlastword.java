7186d241a5fa3d2885a2a00784096adb1852ac6b
serenty <serenty395@gmail.com>
2023-10-08 03:48:29 +0300

    q

--- src/main/java/Algorithms/lengthlastword.java
+++ src/main/java/Algorithms/lengthlastword.java
@@ -1,29 +1,23 @@
 package Algorithms;
 
 import java.util.Scanner;
+
 //21. Дан одномерный массив, который содержит не более 25 положительных и отрицательных целых
 //чисел. Найти наименьший положительный элемент и его номер в массиве.
 //Помогите , пожалуйста(
 public class lengthlastword {
     public static void main(String[] args) {
-        int[] y = { -5 , -7 , -2 , 3, -4, -9  , 2 , 6 , -5};
-        int q = 0;
+        int[] y = { -5, -7, -2, 3, -4, 2 , -9, 2, 6, -5};
+        int q = Integer.MAX_VALUE;
         int index = 0;
-        for (int i = 0; i < y.length; i++) {
-            if(y[i] > 0){
+
+        for (int i = index; i < y.length; i++) {
+            if (y[i] > 0 && q > y[i]) {
                 q = y[i];
                 index = i;
-                break;
             }
         }
-        for (int i = index; i < y.length; i++) {
-            if(y[i] > 0) {
-                if(q > y[i]){
-                    q = y[i];
-                    index = i;
-                }
-            }
-        }
+
 //        for (int i = 0; i < y.length; i++) {
 //            if(y[i] > 0){
 //                q = y[i];
@@ -44,8 +38,6 @@ public class lengthlastword {
         System.out.println(q);
 
 
-
-
 //        Scanner scanner = new Scanner(System.in);
 //        int h = scanner.nextInt();
 //        int[] y = new int[h];
@@ -63,7 +55,12 @@ public class lengthlastword {
 //        }
 
 
+    }
 
-
+    public static int LoopImitation(int[] elements, int index) {
+        if (index == elements.length) {
+            return Integer.MAX_VALUE;
+        }
+        return Math.min(elements[index] < 1 ? Integer.MAX_VALUE : elements[index], LoopImitation(elements, index + 1));
     }
 }
