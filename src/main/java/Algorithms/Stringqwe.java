24a73a5ac77168ce9ce44dd86a7079daa1f55967
serenty <serenty395@gmail.com>
2023-10-14 21:34:30 +0300

    q

---
+++ src/main/java/Algorithms/Stringqwe.java
@@ -0,0 +1,15 @@
+package Algorithms;
+
+import java.util.ArrayList;
+
+public class Stringqwe<T>{
+    T t;
+
+    public Stringqwe(T t) {
+        this.t = t;
+    }
+
+    public void print() {
+        System.out.println(t);
+    }
+}
---
+++ src/main/java/Algorithms/ky.java
@@ -0,0 +1,9 @@
+package Algorithms;
+
+public class ky {
+    public static void main(String[] args) {
+        Stringqwe<String> qwe = new Stringqwe<>("ads");
+        qwe.print();
+        //Q
+    }
+}
